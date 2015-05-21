329978e4119d4f13b5f9be30411e663636f00dd0
zanging <jiushi_530@163.com>
2015-05-21 14:22:43 +0800

    little change

--- WordCount/src/main/java/cn/edu/sjtu/dclab/zzang/topology/WordCounterTopology.java
+++ WordCount/src/main/java/cn/edu/sjtu/dclab/zzang/topology/WordCounterTopology.java
@@ -15,12 +15,12 @@ public class WordCounterTopology {
 		builder.setBolt("counter", new CounterBolt(),1)
 			.fieldsGrouping("splitter", new Fields("word"));
 		
-        //Configuration
+
 		Config conf = new Config();
 		conf.put("filepath", "words.dat");
 		conf.setDebug(true);
-        //Topology run
 		conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+
 		LocalCluster cluster = new LocalCluster();
 		cluster.submitTopology("Word_Counter_Topology", conf, builder.createTopology());
 		Utils.sleep(10000); // wait all the task to finish, or errors come
